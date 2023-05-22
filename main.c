6311807afbf6a0115289adceacd36584a9a31e2c
Mo3110 <niche.hunter.31@gmail.com>
2023-05-22 04:16:32 -0700

    All fixed

--- main.c
+++ main.c
@@ -13,9 +13,10 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -36,8 +37,8 @@ int main(int ac, char **av)
 		}
 		info->readfd = fd;
 	}
-	populateEnvList(info);
-	readHistory(info);
+	populate_env_list(info);
+	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
